7a735966e8528d80da22c8ae57af477a8709055f
Aditya Jadhav <83344372+adityajadhav02@users.noreply.github.com>
2022-08-20 10:00:02 +0530

    Time: 19 ms (72.54%), Space: 13 MB (95.81%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    int found(vector<int> a, int target){
-        for(int i: a)
-            if(i == target) return true;
-        
-        return false;
-    }
-    
-    void setZeroes(vector<vector<int>>& matrix) {
-        vector<int> row, col;
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
+    void setZeroes(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        bool firstRow = false;  // do we need to set first row zero
+        bool firstCol = false;  // do we need to ser first col zero
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    row.push_back(i);
-                    col.push_back(j);
+					if(a[i][j] == 0){   // store rows and cols state in first row and col
+                    if(i==0) firstRow = true;
+                    if(j==0) firstCol = true;
+                    a[i][0] = 0;
+                    a[0][j] = 0;
+                }
+            }
+        }
+        // cout<<firstRow<<" "<<firstCol<<endl;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(a[i][0] == 0 || a[0][j] == 0){
+                    a[i][j] = 0;
                 }
             }
         }
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(found(row, i) || found(col, j))
-                    matrix[i][j] = 0;
-            }
+        if(firstRow){
+            for(int i=0;i<m;i++) a[0][i] = 0;
+        }
+        
+        if(firstCol){
+            for(int i=0;i<n;i++) a[i][0] = 0;
         }
     }
 };
